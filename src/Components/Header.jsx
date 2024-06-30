fb3d7acd8d2dc765654f05d79b46e011876499ac
atikfaisalemon <atikfaisalemon@gmail.com>
2024-06-30 23:29:26 +0600

    responsib for mobile

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import Toggle from "./Toggle";
 export default function Header() {
   return (
     <>
-      <div className="flex items-center justify-between px-56 py-5 bg-white dark:bg-slate-600">
+      <div className="flex items-center sm:justify-between sm:px-56 py-5 bg-white dark:bg-slate-600">
         <div className="w-max flex items-center justify-between">
           <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-orange-500 pr-5 text-5xl text-black dark:text-white font-bold">
             Text-Converter
